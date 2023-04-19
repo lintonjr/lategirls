8bf33f20be45c7516de9c6cef6af80022e15c713
Linton Junior <lintonjr@hotmail.com>
2023-04-19 18:44:52 -0400

    lab login

--- app/src/main/java/com/motoacademy/lablogin/MainActivity.java
+++ app/src/main/java/com/motoacademy/lablogin/MainActivity.java
@@ -1,19 +1,51 @@
 package com.motoacademy.lablogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText txtEmail, txtPass;
+    MaterialButton btnEntrar;
+    FirebaseAuth mAuth;
     TextView gotoRegister;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        txtEmail = findViewById(R.id.edemail);
+        txtPass = findViewById(R.id.edpass);
+        btnEntrar = findViewById(R.id.btnEntrar);
         gotoRegister = findViewById(R.id.regNow);
 
         gotoRegister.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +56,42 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, passwd;
+
+                email = txtEmail.getText().toString();
+                passwd = String.valueOf(txtPass.getText());
+
+                if(TextUtils.isEmpty(email)) {
+                    Toast.makeText(MainActivity.this, "O email não pode estar vazio", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(passwd)) {
+                    Toast.makeText(MainActivity.this, "Senha não pode estar vazio", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(email, passwd)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d("Success", "signInWithEmail:success");
+                                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("failed", "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
     }
 }
--- app/src/main/java/com/motoacademy/lablogin/Register.java
+++ app/src/main/java/com/motoacademy/lablogin/Register.java
@@ -1,30 +1,55 @@
 package com.motoacademy.lablogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class Register extends AppCompatActivity {
 
     EditText txtEmail, txtPass;
     MaterialButton btnReg;
     TextView gotoLogin;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_register);
         txtEmail = findViewById(R.id.edemail);
         txtPass = findViewById(R.id.edpass);
         btnReg = findViewById(R.id.btnEntrar);
         gotoLogin = findViewById(R.id.logNow);
+        progressBar = findViewById(R.id.progressbar);
 
         gotoLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +64,7 @@ public class Register extends AppCompatActivity {
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
                 String email, passwd;
 
                 email = txtEmail.getText().toString();
@@ -53,6 +79,25 @@ public class Register extends AppCompatActivity {
                     Toast.makeText(Register.this, "Senha não pode estar vazio", Toast.LENGTH_SHORT).show();
                     return;
                 }
+
+                mAuth.createUserWithEmailAndPassword(email, passwd)
+                        .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    progressBar.setVisibility(View.GONE);
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("TAG", "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                } else {
+                                    progressBar.setVisibility(View.GONE);
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("Failed", "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(Register.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
     }
